41222e5a5484b88b0f6219ed68f72e122f5ec417
黄雨生 <yusen@HysdeMacBook-Pro.local>
2017-03-30 13:05:30 +0800

    视图更新

--- views/return.js
+++ views/return.js
@@ -12,7 +12,19 @@ $(document).ready(function(){
             $("#return").hide();
             $("#return").html("");
             for(var i = 0;i < data.bno.length;i++){
-                var s = '<p class="data">书号:' + data.bno[i] + ", 书名:" + data.title[i] + ', 类型:' + data.type[i] + ", 作者:" + data.author[i] + ", 出版社:" + data.press[i] + ", 出版日期:" + data.year[i] + ", 价格:" + data.price[i].toString() + ", 总量:" + data.total[i].toString() + ", 库存:" + data.stock[i].toString() + "</p>";
+                var s = '<p class="data">书号:' + data.bno[i] + ", 书名:" + data.title[i] + ', 类型:' + data.type[i] + ", 作者:" + data.author[i] + ", 出版社:" + data.press[i] + ", 出版日期:" + data.year[i] + ", 价格:" + data.price[i].toString();
+                if(data.total[i] != null){
+                    s = s + ", 总量:" + data.total[i].toString();
+                }
+                else{
+                    s = s + ", 总量:" + "0";
+                }
+                if(data.stock[i] != null){
+                    s = s + ", 库存:" + data.stock[i].toString() + "</p>";
+                }
+                else{
+                    s = s + ", 库存:" + "0";
+                }
                 $("#return").append(s);
             }
             $("#return").show(1000);
